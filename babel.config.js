dfe23dcd2fcaba1f3d54a90fcb3490d0a4daabc2
Nicolas AUGER <nicolas@poool.fr>
2020-01-27 18:26:11 +0100

    chore: set babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    ['@babel/env', {
+      corejs: 3,
+      useBuiltIns: 'usage',
+    }],
+    '@vue/cli-plugin-babel/preset',
+  ],
+  plugins: [
+    ['@babel/transform-runtime', {
+      corejs: 3,
+      helpers: true,
+      regenerator: true,
+      useESModules: false,
+    }],
+    '@babel/proposal-optional-chaining',
+    '@babel/proposal-object-rest-spread',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-nullish-coalescing-operator',
+  ],
+};
