ae27053e497821811b4529e930f6e1ac547eb6da
Nicolas AUGER <nicolas@poool.fr>
2020-01-28 12:09:30 +0100

    feat: add camera on desk view

--- src/components/Desk.vue
+++ src/components/Desk.vue
@@ -13,12 +13,19 @@
         <PageTitle :text="pageTitle" :icon="pageIcon"/>
       </div>
     </div>
+
+    <main v-on:load="cameraStart" id="camera">
+        <video id="camera-view" autoplay playsinline></video>
+        <img src="//:0" alt="" id="camera-output">
+    </main>
   </div>
 </template>
 
 <script>
 import BreadCrumb from './fragments/BreadCrumb.vue'
 import PageTitle from './fragments/PageTitle.vue'
+
+const constraints = { video: { facingMode: "user" }, audio: false }
 
 export default {
   name: 'Desk',
@@ -29,8 +36,32 @@ export default {
       pageIcon: 'web',
     }
   },
+  methods: {
+    cameraStart() {
+      navigator.mediaDevices
+          .getUserMedia(constraints)
+          .then(function(stream) {
+            const cameraView = document.querySelector("#camera-view")
+            const track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+      })
+      .catch(function(error) {
+          console.error("Oops. Something is broken.", error);
+      });
+    }
+  },
+  mounted: function() {
+    this.cameraStart()
+  },
   components: { BreadCrumb, PageTitle },
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+#camera, #camera-view
+  height: auto
+  object-fit: cover
+
+#camera-view
+  transform: scaleX(-1)
+</style>
