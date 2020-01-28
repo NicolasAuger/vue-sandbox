8d66316a4e5a8c293252165b37d3b725a64e1728
Nicolas AUGER <nicolas@poool.fr>
2020-01-28 01:20:17 +0100

    refactor: better routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Desk from './components/Desk.vue';
 import Todos from './components/Todos.vue';
 
 export const routes = [
-  { path: '/', component: Desk },
-  { path: '/todos', component: Todos },
-  { path: '*', component: Desk },
+  { path: '/', component: Todos },
+  { path: '/desk', component: Desk },
+  { path: '*', redirect: '/' },
 ];
