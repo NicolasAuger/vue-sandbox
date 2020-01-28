1763fd584fe515818b15eafa2455f7a5a0e00d65
Nicolas AUGER <nicolas@poool.fr>
2020-01-28 01:21:35 +0100

    feat: add breadcrumb & pagetitle vue fragments

---
+++ src/components/fragments/BreadCrumb.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ui-breadcrumb">
+    <span v-for="item in items" :key="item">{{ item }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.ui-breadcrumb
+  cursor: default
+
+  span
+    color: #000A26
+
+    &:after
+      content: ">"
+      margin: 0 5px
+      display: inline
+      position: relative
+      top: 1px
+
+    &:last-child
+      font-weight: bold
+      color: #0E96EF
+
+      &:after
+        display: none
+</style>
---
+++ src/components/fragments/PageTitle.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2 class="ui-page-title">
+    <div class="row justify-content-between">
+      <div class="col-auto">
+        <div class="d-flex align-items-center">
+          <i class="material-icons md-36">{{ icon }}</i>
+          <span>{{ text }}</span>
+        </div>
+      </div>
+    </div>
+  </h2>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../../theme/ui/colors.styl"
+
+h2.ui-page-title
+  color: $color-midnight
+  font-size: 36px
+  font-weight: normal
+  margin-bottom: 20px
+  margin-top: 10px
+
+  i
+    margin-right: 5px
+    width: 50px
+
+  span
+    margin-right: 20px
+</style>
