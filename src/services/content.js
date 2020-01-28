914314ba7f68589b1acd55cc776d72371a2a78b9
Nicolas AUGER <nicolas@poool.fr>
2020-01-28 01:20:30 +0100

    feat: add content service

---
+++ src/services/content.js
@@ -0,0 +1,17 @@
+export const menus = [
+  {
+    name: 'Tests',
+    menu: [
+      {
+        name: 'TodoList',
+        route: '/',
+        icon: 'assignment_turned_in',
+      },
+      {
+        name: 'Desk',
+        route: '/desk',
+        icon: 'web',
+      },
+    ],
+  },
+];
