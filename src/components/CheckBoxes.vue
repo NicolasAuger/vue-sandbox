574d857b178415e300de93dda061f3582bd7f5d4
Nicolas AUGER <nicolas@poool.fr>
2020-04-03 17:52:30 +0200

    feat: add checkboxes vue

---
+++ src/components/CheckBoxes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="page components checkboxes">
+    <div class="row">
+      <div class="col col-12">
+        <BreadCrumb
+          class="text-left"
+          :items="breadCrumb"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-12">
+        <PageTitle :text="pageTitle" :icon="pageIcon"/>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col col-12">
+        <CheckBox>
+          This is a checkbox
+        </CheckBox>
+        <CheckBox :disabled="true">
+          This is a disabled checkbox
+        </CheckBox>
+        <CheckBox theme="none">
+          This is an unthemed checkbox
+        </CheckBox>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BreadCrumb from './fragments/BreadCrumb.vue'
+import PageTitle from './fragments/PageTitle.vue'
+import CheckBox from './elements/CheckBox.vue'
+
+export default {
+  name: 'CheckBoxes',
+  data() {
+    return {
+      breadCrumb: ['Home', 'Components', 'CheckBoxes'],
+      pageTitle: 'CheckBoxes',
+      pageIcon: 'check',
+    }
+  },
+  components: { BreadCrumb, CheckBoxes, PageTitle },
+}
+</script>
+
+<style lang="stylus" scoped></style>
